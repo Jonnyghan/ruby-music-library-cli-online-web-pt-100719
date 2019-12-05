ee931ebd7e76e1a19c28c3ad669bbf0741f5fafd
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-12-05 18:50:41 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -28,7 +28,12 @@ class MusicLibraryController
     list_artists
   when "list genres"    
   list_genres
-  
+  when "list artist"
+  list_songs_by_artist
+  when "list genre"
+  list_songs_by_genre
+  when "play song"
+  play_song
   
   else 
     "Type in a valid request please"
