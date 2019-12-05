c27ade9fe2e883b4342a24993596a9b64db424d2
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-12-05 18:11:19 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -8,6 +8,7 @@ class MusicLibraryController
 
   def call
     input =""
+    while input != "exit"
   puts "Welcome to your music library!"  
   puts "To list all of your songs, enter 'list songs'."
   puts "To list all of the artists in your library, enter 'list artists'."
@@ -19,6 +20,7 @@ class MusicLibraryController
   puts "What would you like to do?"
   
   input= gets.chomp
+    end
   end
    
    
