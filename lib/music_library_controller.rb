eb33a6b33078a93a30bf80811a3c635eebbef1a5
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-12-05 18:25:00 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -29,7 +29,7 @@ class MusicLibraryController
      end
    end
    
-   def list artists
+   def list_artists
      Artist.all.sort {|a,b| a.name <=> b.name }.each.with_index(1) do |artist,num|
        puts "#{num}. #{artist.name}"
      end
